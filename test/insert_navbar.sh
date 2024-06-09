7e4036f3db23969123430e49bab87affb238645b
Shravan Goswami <shravanngoswamii@gmail.com>
2024-06-09 17:38:24 +0530

    testing

--- test/insert_navbar.sh
+++ test/insert_navbar.sh
@@ -15,10 +15,13 @@ if [ -z "$NAVBAR_HTML" ]; then
     exit 1
 fi
 
+# Escape special characters for inserting into HTML files
+ESCAPED_NAVBAR_HTML=$(echo "$NAVBAR_HTML" | sed -e 's/[\/&]/\\&/g')
+
 # Process each HTML file in the directory
 for file in $(find $HTML_DIR -name "*.html"); do
     if grep -q "<body>" "$file"; then
-        sed -i "/<body>/a $NAVBAR_HTML" "$file"
+        awk -v navbar="$ESCAPED_NAVBAR_HTML" '1;/<body>/{print navbar}' "$file" > temp && mv temp "$file"
         echo "Updated $file"
     else
         echo "<body> tag not found in $file"
