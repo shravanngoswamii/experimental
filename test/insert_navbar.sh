38a270e4c423fa0041d0680451a8b30eecc875e5
Shravan Goswami <shravanngoswamii@gmail.com>
2024-06-22 08:03:49 +0530

    test1

--- test/insert_navbar.sh
+++ test/insert_navbar.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+
 # This script inserts a top navigation bar (e.g., `navbar.html`) into Documenter.jl generated sites.
+# The resulting output is similar to MultiDocumenter's navigation menu. The navigation menu is
+# hard-coded at the moment, which could be improved in the future. 
 # It checks all HTML files in the specified directory and its subdirectories.
-# The script updates the navbar if a new version is available.
+# The script also avoids inserting the navbar if it's already present.
 
 # URL of the navigation bar HTML file
-NAVBAR_URL="https://raw.githubusercontent.com/shravanngoswamii/experimental/main/test/navbar.html"
+NAVBAR_URL="https://raw.githubusercontent.com/TuringLang/turinglang.github.io/main/assets/scripts/navbar.html"
 
 # Directory containing HTML files (passed as the first argument to the script)
 HTML_DIR=$1
-
-# New navbar version (passed as the second argument to the script)
-NEW_VERSION=$2
 
 # Download the navigation bar HTML content
 NAVBAR_HTML=$(curl -s $NAVBAR_URL)
@@ -23,23 +23,18 @@ fi
 
 # Process each HTML file in the directory and its subdirectories
 find "$HTML_DIR" -name "*.html" | while read file; do
-    # Check if the navbar is already present and get its version
-    CURRENT_VERSION=$(grep -oP '(?<=<!-- NAVBAR START v)[^[:space:]]+(?= -->)' "$file")
-    
-    if [ -z "$CURRENT_VERSION" ] || [ "$CURRENT_VERSION" != "$NEW_VERSION" ]; then
+    # Check if the navbar is already present using the comment
+    if ! grep -q "<!-- NAVBAR START -->" "$file"; then
         # Read the contents of the HTML file
         file_contents=$(cat "$file")
         
-        # Remove existing navbar if present
-        file_contents=$(echo "$file_contents" | sed '/<!-- NAVBAR START v[^[:space:]]* -->/,/<!-- NAVBAR END -->/d')
-        
-        # Insert the new navbar HTML after the <body> tag
+        # Insert the navbar HTML after the <body> tag
         updated_contents="${file_contents/<body>/<body>$NAVBAR_HTML}"
         
         # Write the updated contents back to the file
         echo "$updated_contents" > "$file"
-        echo "Updated $file to navbar version $NEW_VERSION"
+        echo "Updated $file"
     else
-        echo "Skipped $file (navbar already at version $CURRENT_VERSION)"
+        echo "Skipped $file (navbar already present)"
     fi
 done
