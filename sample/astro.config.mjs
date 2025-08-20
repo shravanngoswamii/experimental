68502db6e1d5146b33be565044412659e6d24b54
Shravan Goswami <shravanngoswamii@gmail.com>
2025-08-20 17:06:58 +0530

    fix GHA

--- sample/astro.config.mjs
+++ sample/astro.config.mjs
@@ -1,16 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-/**
- * Astro configuration file.
- * @see https://astro.build/config
- */
+// https://astro.build/config
 export default defineConfig({
-  // The final domain where your site will be deployed.
-  // This is crucial for generating correct canonical URLs and sitemaps.
   site: 'https://shravangoswami.com',
-
-  // The subdirectory your site lives in.
-  // This must match the repository name and any subfolders in your deployment.
+  // The base should be the full path *after* the domain
   base: '/experimental/washer/',
 });
