f66b75b5314c7a0bff73b205b61c11f39f48f996
Shravan Goswami <shravanngoswamii@gmail.com>
2025-08-20 16:03:30 +0530

    .

--- sample/astro.config.mjs
+++ sample/astro.config.mjs
@@ -1,8 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  // Your root domain
   site: 'https://shravangoswami.com',
+
+  // The full path from the domain root to your site
   base: '/experimental/washer/',
 });
