ad36c513c2082160c2a1d5f3ea8ed3660670c07f
Shravan Goswami <shravanngoswamii@gmail.com>
2026-04-15 09:32:22 +0530

    Add Format workflow test with unformatted Julia sample

---
+++ julia-samples/example.jl
@@ -0,0 +1,22 @@
+module Example
+
+# Badly formatted Julia code for testing JuliaFormatter
+
+function add(x,y)
+return x+y
+end
+
+function greet( name )
+    println( "Hello, ", name,"!" )
+end
+
+struct Point
+x::Float64
+y::Float64
+end
+
+function distance(p1::Point,p2::Point)
+    sqrt((p1.x-p2.x)^2+(p1.y-p2.y)^2)
+end
+
+end
